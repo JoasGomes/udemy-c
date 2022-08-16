35505f95c62b8dd53c69c667d8a7d262b3d64950
JoasGomes <jojoca.vitor777@gmail.com>
2022-08-16 05:11:41 -0300

    iniciando c++

---
+++ aula21.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  std::string palavra;
+
+  // imprimindo na tela
+  cout << "digite uma palavra";
+
+  // lendo uma string
+  cin >> palavra;
+
+  printf("digite uma palavra");
+
+  // std::cin >> palavra;
+  // std::cout << palavra;
+
+  // imprimindo uma variável
+  cout << "\n palavra eh:" << palavra;
+}
